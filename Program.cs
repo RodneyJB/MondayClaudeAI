0780dfe90a785347116b161c65e3a5ad0fd79b44
Rodney Buljubasic <rodney@bootepolch.local>
2026-05-08 13:34:19 +0200

    Added Monday GraphQL service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MondayClaudeAI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,5 +11,6 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.MapControllers();
+builder.Services.AddHttpClient<MondayService>();
 
 app.Run();
