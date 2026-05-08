4919dd72a719f73a878fcd73c1e3290ce1b9042d
Rodney Buljubasic <rodney@bootepolch.local>
2026-05-08 10:20:16 +0200

    Fix Render startup

--- Program.cs
+++ Program.cs
@@ -3,17 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.MapControllers();
 
