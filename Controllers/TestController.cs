a619c80e53a5d06cfecf8b5638b151ceda2fd871
Rodney Buljubasic <rodney@bootepolch.local>
2026-05-08 13:47:35 +0200

    Added test columns endpoint

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using MondayClaudeAI.Services;
+
+namespace MondayClaudeAI.Controllers;
+
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly MondayService _monday;
+
+    public TestController(MondayService monday)
+    {
+        _monday = monday;
+    }
+
+    [HttpGet("/test-columns/{boardId}")]
+    public async Task<string> Test(long boardId)
+    {
+        var token = Environment.GetEnvironmentVariable("MONDAY_API_TOKEN");
+
+        return await _monday.GetBoardColumns(boardId, token!);
+    }
+}
